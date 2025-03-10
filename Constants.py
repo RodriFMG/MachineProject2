0ce76e261dafee01d8c6cc428803bfd54d69f057
RODRIGO <rodrigo.meza@utec.edu.pe>
2025-03-10 18:46:54 -0500

    pe

---
+++ Constants.py
@@ -0,0 +1,7 @@
+DataDicEjes = {
+        "acc": ["body_acc_x", "body_acc_y", "body_acc_z"],
+        "total": ["total_acc_x", "total_acc_y", "total_acc_z"],
+        "gyro": ["body_gyro_x", "body_gyro_y", "body_gyro_z"]
+}
+
+DataPath = "C:/Users/RODRIGO/PycharmProjects/MachineProject2/Data1/train.h5"
--- main.py
+++ main.py
@@ -2,24 +2,59 @@ import h5py
 import numpy
 import numpy as np
 
+from Constants import DataDicEjes, DataPath
+
+
 # TypeData:
 
 # acc: retorna una lista de numpy con el eje X, Y, Z de body_acc
-# gyro: retorna una lista de numpy con el eje X, Y, Z de
+# gyro: retorna una lista de numpy con el eje X, Y, Z de body_gyro
+# total: retorna una lista de numpy con el eje X, Y, Z de total_acc
 def ObtenerData(DataPath, TypeData):
-    x = []
-    y = []
+    Ejes = DataDicEjes[TypeData]
+    Data = []
+    Labels = []
 
-    Data = {
-        "acc" : [ "", "", ""]
-    }
+    with h5py.File(DataPath, "r") as f:
+        for clave in f.keys():
 
-    Data = "C:/Users/RODRIGO/PycharmProjects/MachineProject2/Data1/train.h5"
+            # Para realizar un proceso similar al list.include(data) de js, pues
+            # directamente es con if data in PythonList, en python. ( mismo proceso que
+            # el include )
+            if clave in Ejes:
+                Data.append(f[clave][:])
+            elif clave == "y":
+                Labels.append(f[clave][:])
 
-x = []
-y = []
+    # Para verificar que una lista está vacio, es con if not ListPython, o len(ListPython)== 0
+    if not Data:
+        print("Se colocó la etiqueta, solo se accede con: "
+              f"acc, gyro o total. Se colocó: {TypeData}")
+        exit(0)
 
-with h5py.File(Data, "r") as f:
-    for clave in f.keys():
-        xd = np.array(f[clave][:])
-        print(clave)
+    # Data: Array con los ejes de los cálculos realizados en las acciones
+    # Labels: Array con las etiquetas correctas de las acciones.
+
+    x = np.array(Data)
+    y = np.array(Labels)
+
+
+    x = np.transpose(x, (1, 0, 2))
+    y = np.reshape(y, (-1))
+    return x, y
+
+
+x, y = ObtenerData(DataPath, "acc")
+
+# x.shape: Cálculos realizados en la actividad de la muestra.
+
+# [0]: # de muestras
+# [1]: # de ejes (eje x, y, z)
+# [2]: todos los calculos realizados en ese eje para esa muestra
+
+# y.shape: Etiqueta real de la actividad realizada ( representación de la activdad real
+# del vector x.
+
+# [0]: # de muestras.
+
+print(x.shape, y.shape)
